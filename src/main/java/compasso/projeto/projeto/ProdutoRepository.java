279dad4c80b0c7f30c458c35d48963dbe8f9f9c5
Danielpassamanidp <86212026+Danielpassamanidp@users.noreply.github.com>
2021-12-09 20:48:15 -0300

    Delete src directory

--- src/main/java/compasso/projeto/projeto/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package compasso.projeto.projeto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<produto, Long>{
-	
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO produto(description, name, price) VALUES ('Modelo carro', 'Gol', 2444.87);
-
-INSERT INTO produto(description, name, price) VALUES ('Modelo carro', 'Ferrari', 2555.30);
